e57fc2a76523a7abe302d317710abc40efe0aa2c
mamenDseid <mamelala@gmail.com>
2022-09-27 14:14:07 +0000

    sd

---
+++ build/precache-manifest.87bfcf464505e3093eaea213c07a7ba3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4756c636e1ad7253056206c16aeabf2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31fa6ca30f73f45ff351",
+    "url": "/static/js/2.a7a30fae.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.a7a30fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ef169be1a83d373ac5",
+    "url": "/static/js/main.44c47536.chunk.js"
+  },
+  {
+    "revision": "1cba36deef3a7d2b858a",
+    "url": "/static/js/runtime-main.00c5e145.js"
+  }
+]);
